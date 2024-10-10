7e84989cef7f64e07ba84ee87f3b7bfd86bd9dbd
IgorLTS10 <igorchrzyptowicz@myges.fr>
2024-10-10 09:13:22 +0200

    Initialized TokenA

--- test/TokenA.t.sol
+++ test/TokenA.t.sol
@@ -1,10 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../src/TokenA.sol";
 
-contract TokenA is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Token A", "TKNA") {
-        _mint(msg.sender, initialSupply);
+contract TokenATest is Test {
+    TokenA tokenA;
+
+    function setUp() public {
+        // On déploie le contrat TokenA avec un montant initial de 1 000 tokens
+        tokenA = new TokenA(1000 * 10 ** 18);
+    }
+
+    function testInitialSupply() public view {
+        // Vérifie que le montant initial de tokens est attribué correctement au créateur du contrat
+        assertEq(tokenA.totalSupply(), 1000 * 10 ** 18);
+        assertEq(tokenA.balanceOf(address(this)), 1000 * 10 ** 18);
+    }
+
+    function testTokenNameAndSymbol() public view{
+        // Vérifie le nom et le symbole du token
+        assertEq(tokenA.name(), "Token A");
+        assertEq(tokenA.symbol(), "TKNA");
+    }
+
+    function testTransfer() public {
+        // Teste un transfert de tokens vers une adresse spécifique
+        address recipient = address(0xBEEF);
+        tokenA.transfer(recipient, 100 * 10 ** 18);
+
+        assertEq(tokenA.balanceOf(recipient), 100 * 10 ** 18);
+        assertEq(tokenA.balanceOf(address(this)), 900 * 10 ** 18);
     }
 }
